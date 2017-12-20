aa609f53cabeeac2ac58bf07961ecdb68de95add
Eniong <Eniong@github.com>
2017-12-21 01:48:05 +0800

    OSA

---
+++ platforms/windows/www/javascripts/fcfs.js
@@ -0,0 +1,22 @@
+﻿function fcfs() {
+	var process_list = new ProcessList();
+
+	//iterate to all process and build them in a list
+	$inputs_container.children().each(function() {
+		  var process_name = $(this).find('.form-control.process-name').val();
+		  var arrival_time = parseInt($(this).find('.form-control.arrival-time').val());
+		  var burst_time = parseInt($(this).find('.form-control.burst-time').val());
+
+		  var current_process = new Process(process_name, arrival_time, burst_time);
+		  process_list.add(current_process);
+		});
+		
+	//sort process according to there arival time & build the chart
+	process_list.sort('arival_time');
+	var arr = process_list.avg_turn_around_and_wating_time();
+
+	$('.avarage-time').html(arr[0]);
+	$('.turn-around-time').html(arr[1]);
+	build_chart(process_list);
+	// $('.avarage-time').html(process_list.get_average_time());
+}
---
+++ www/javascripts/main.js
@@ -0,0 +1,48 @@
+//update dropdown menu text when clicking on one of its items
+$('ul.scheduler-type li').click(function() {
+	scheduler_type = $(this).text().trim();
+	var html = scheduler_type+' <span class="caret"></span>';
+	$scheduler_type_btn.html(html);
+
+	$options_form.empty();
+	    if(scheduler_type == "Priority") {
+	    	$options_form.html(options);    	
+	    	$(".options input[name='options']:radio").change(function() {
+				priority();
+			});
+	    } else if( scheduler_type == "SJF") {
+	    	$options_form.html(options);    	
+	    	$(".options input[name='options']:radio").change(function() {
+	    		sjf();
+			});
+	    } else if( scheduler_type == "Round Robin") 
+	    	$options_form.html(time_quantum_txtfield);   
+});
+
+
+// update inputs numbers to matche with processes numbers
+$(".processes-number").bind('keypress', function(e){
+	    // var keycode = (event.keyCode ? event.keyCode : event.which);}
+    scheduler_type = $scheduler_type_btn.text().trim();
+    // check if the pressed key is 'enter'
+    if(e.keyCode == '13'){
+    	//clear all exisiting process and generate a new ones
+        $inputs_container.empty();
+        if(scheduler_type == "Priority")
+	        for(var i = 0; i < $(this).val(); i++) 
+	        	$inputs_container.append(priority_div);
+	    else
+	    	for(var i = 0; i < $(this).val(); i++) 
+	        	$inputs_container.append(fcfs_div);
+    }
+});
+
+$('#run-div').delegate('.btn.run', 'click', function() {
+	scheduler_type = $scheduler_type_btn.text().trim();
+	switch(scheduler_type) {
+		case 'FCFS': fcfs(); break;
+		case 'SJF' : sjf();  break;
+		case 'Priority': priority(); break;
+		case 'Round Robin': round_robin(); break;
+	}
+});
